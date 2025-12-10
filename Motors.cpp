b5c87cd6bace5dc9d8ad3c9c4846d2f0dc2c8da1
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 23:19:28 +0100

    Create Motors.cpp with implementation notes
    Add placeholder comments for future implementation of ESP32 ledC control for BLDC ESCs.

---
+++ Motors.cpp
@@ -0,0 +1,6 @@
+/* 
+Placeholder file for now, will be used later for implementation of the ESP32 ledC (PWM) control for BLDC ESCs
+the way the rest of the code is programmed there is a high chance it will only work with arduino core 3.0+m most information about it found with these links:
+https://github.com/espressif/arduino-esp32
+https://docs.espressif.com/projects/arduino-esp32/en/latest/
+*/

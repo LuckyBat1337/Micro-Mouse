31145ac684b61f63afdc594dbd00fdd2ab9f8977
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 14:36:44 +0100

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -5,9 +5,12 @@ Det ska göras genom att undersöka varvtalet i en borstlös ( ZZ ) DC motor.
 #include <iostream>
 #include <string>
 
+int x=0;
 string cardinalDirection[4] = {"NORTH", "EAST", "SOUTH", "WEST"};
+string (&curDir)[4] = currentDirection;
 string (&carDir)[4] = cardinalDirection;
 
+string carDir[0] = 
 int main(){
 
 }

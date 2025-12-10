f6f0f7c641a129894494d7081ae632509328352a
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 14:46:07 +0100

    Add constant variables for robot specifications
    Defined constant variables for wheel dimensions, gear ratio, encoder counts, track width, maze size, and total cells.

--- constantVariables.cpp
+++ constantVariables.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <string>
+const int WHEEL_DIAMATER = ZZ; // Diameter of the wheel
+const int WHEEL_RADIUS = ZZ; // Radius of the wheel
+const int GEAR_RATIO = ZZ; // Gear ratio, example 10:1, used for encoder resolution
+const int ENCODER_CPR = ZZ; // Stands for Encoder Counts Per Revolution
+const int TRACK_WIDTH = ZZ; // Likely 16cm width because of 18cm max possible size - (1 * 2)cm for walls
+const int MAZE_SIZE = 288; // Maze size in centimeters
+const int TOTAL_CELLS = 256; // Total amount of cells in the cell labyrint (16x16)
 

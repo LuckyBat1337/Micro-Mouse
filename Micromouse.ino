380f09a336d253b628cd8ba9f069b1e99d487c71
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 23:38:40 +0100

    Initialize serial and implement basic movement logic
    Added initialization for serial communication and motors, and implemented basic movement logic for the robot.

--- Micromouse.ino
+++ Micromouse.ino
@@ -10,9 +10,34 @@
 #endif
 
 void setup(){
-
+/*   not 100% sure on 115200, according to google it is the max rate a ESP32 can read at, 
+     "initializes the ESP32's default hardware serial port (UART0) for communication at a speed of 115,200 bits per second (baud)" 
+     https://randomnerdtutorials.com/esp32-uart-communication-serial-arduino/#:~:text=When%20you%20use%20Serial.,UART%201%20and%20UART%202.
+*/
+ Serial.begin(115200);
+ #ifndef SIMULATOR_MODE
+    Motors::init();
+    Sensors::init();
+ #else
+    //simulator initalization, cant be bothered will do later ZZ
+ #endif
+ API::ackReset();
 }
 
 void loop(){
-
+//algorithm section, runs same on real robot and simulator.
+//simple simple algorithm to test later if anything will move in a simulation
+    if(!API::wallLeft()){
+        API::turnLeft();
+        API::moveForward();
+    }
+    else if (!API::wallFront()){
+        API::moveForward();
+    }
+    else {
+        API::turnRight(); //added this bit incase it gets stuck in a dead end or corner
+                          // |-------------
+                          // | Ö                   Robot will turn right if this happens because there is a wall on the left and a wall in front forcing the right move (Ö is the mouse)
+                          // |
+    }
 }

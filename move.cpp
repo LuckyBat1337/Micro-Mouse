c117dc734d46aa673219506802cc2b0bec30d029
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 13:19:01 +0100

    Correct syntax and define cardinal directions
    Fix syntax errors in move.cpp and add cardinal directions.

--- move.cpp
+++ move.cpp
@@ -2,3 +2,7 @@
 Det behövs för att se till att den ska förflytta sig med hjälp av primitiva funktionen för att få distansen
 Det ska göras genom att undersöka varvtalet i en borstlös ( ZZ ) DC motor. 
 */
+#include iostream
+
+string cardinalDirection[4] = {NORTH, EAST, SOUTH, WEST)
+

e3c7dd973dd14dd819cdb16143ca066e5450f7b0
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 23:28:06 +0100

    Initialize Micromouse.ino with includes and setup
    Added necessary includes and setup for Arduino project.

--- Micromouse.ino
+++ Micromouse.ino
@@ -1,3 +1,18 @@
 //  This file is "main.cpp", arduino looks for a file with file ending ".ino" which is why it has been renamed to Micromouse.ino (from my understanding)
 
+#include <Arduino.h> //various things needing inclusion, arduino for string and boolean and such replacing iostream (im not sure about this? ZZ )
+#include "Configuration.h"
+#include "API.h"
 
+#ifndef SIMULATOR_MODE
+    #include "Motors.h"
+    #include "Sensors.h"
+#endif
+
+void setup(){
+
+}
+
+void loop(){
+
+}

2200f9662cb6ccfe8003a7890b8a287132829191
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 23:12:56 +0100

    Create Motors.h for motor control abstraction
    Add placeholder for Motors.h with hardware abstraction notes

---
+++ Motors.h
@@ -0,0 +1,6 @@
+/*
+For now no time to add but file needs to exist for running, what I want here is hardware abstraction for the brushless DC motors via ESCs, also should include handling of PWM generation and kinematics.
+Hopefully Leo/Maja can program this during one of our next lessons?
+
+
+*/

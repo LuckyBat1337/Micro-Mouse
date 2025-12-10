9c2b24a5bf4765cc53cbb6f8b2dd3d1c184de977
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 23:26:01 +0100

    Update Micromouse.ino

--- Micromouse.ino
+++ Micromouse.ino
@@ -1,3 +1,3 @@
-/*  This file is "main.cpp", arduino looks for a file with file ending ".ino" which is why it has been renamed to Micromouse.ino (from my understanding)
+//  This file is "main.cpp", arduino looks for a file with file ending ".ino" which is why it has been renamed to Micromouse.ino (from my understanding)
 
-*/
+

e416438ae6a9c79e36f07e967be70ff169d4b4f1
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-10 12:58:00 +0100

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -1,5 +1,4 @@
 /*
 Det behövs för att se till att den ska förflytta sig med hjälp av primitiva funktionen för att få distansen
 Det ska göras genom att undersöka varvtalet i en borstlös ( ZZ ) DC motor. 
-
 */

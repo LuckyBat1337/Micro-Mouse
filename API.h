1a0a3e2514deee72c630cea425c1e9e976d32a65
Aiden Koczynski <49494043+AidenKoczynski@users.noreply.github.com>
2025-12-11 10:51:07 +0100

    Update API.h

--- API.h
+++ API.h
@@ -1,30 +1,31 @@
-// API.h
 #pragma once
 #include <string>
 
 class API {
 public:
-    // Sensing Methods
     static int mazeWidth();
     static int mazeHeight();
+
     static bool wallFront();
     static bool wallRight();
     static bool wallLeft();
-    
-    // Movement Methods
+    static bool wallBack();
+
     static void moveForward(int distance = 1);
+    static void moveForwardHalf(int distance = 1);
     static void turnRight();
     static void turnLeft();
-    
-    // Visualization/Debug Methods
+
     static void setWall(int x, int y, char direction);
     static void clearWall(int x, int y, char direction);
     static void setColor(int x, int y, char color);
     static void clearColor(int x, int y);
+    static void clearAllColor();
+
     static void setText(int x, int y, const std::string& text);
     static void clearText(int x, int y);
-    
-    // State Management
+    static void clearAllText();
+
     static bool wasReset();
     static void ackReset();
 };
